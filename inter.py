52e172a0067c6636bca5fcad821b1e2853a8bac8
StevennTran <37421431+StevennTran@users.noreply.github.com>
2021-04-14 19:34:02 -0400

    Got the dupilcate symbol entries working

--- inter.py
+++ inter.py
@@ -1,83 +1,102 @@
 def generate(tokens,symbol_table):
     final_string = ""
-    triples = {}
+    # triples = {}
 
     #   OP | arg1 | arg2 | res
     #   =  |  2   |  -   | t1
     #   +  |  t1  |  1   | t2
     #  ret |  t2  |  -   |    
     var_count = 1
+    label_count = 1
 
     # To get from symbol value
     value_dic = {}
     var_maps = {}
     quadruples = []
-    insert_flag = False
+    while_insert_flag = False
     op = ""
-    for curr_token in tokens:
-        
+    for i in range (len(tokens)):
+    # for curr_token in tokens:
+        curr_token = tokens[i]
+
         if (curr_token[0] == "func_decl"):
             final_string += curr_token[1]+":\n"
-        elif (curr_token[0] == "dataType"):
-            insert_flag = True
+
+        elif (curr_token[0] == "while_loop"):
+            quadruples.append(("while","label{}:".format(label_count),"",""))
+            label_count += 1
+            while_insert_flag = True
+        
+
+
+        # elif while_insert_flag == True:
+        #     if curr_token[0] == "bracket" and curr_token[1] == "}":
+                
+
         elif (op == "ret" and curr_token[0] == "id"):
             quadruples.append((op,var_maps[curr_token[1]],"",""))
         # elif (curr_token[0])
-        elif (curr_token[0] == "id" and insert_flag):
+        elif (curr_token[0] == "id"):
             op = "="
             # print(curr_token[1])
-            token_exists = 0
-            for each in quadruples:
-                if (curr_token[-1] in var_maps):
-                    if (each[-1] == var_maps[curr_token[1]]):
-                        token_exists = 1
-                        break
 
-            if (token_exists == 0):
-                inter_var = "t"+ str(var_count)
-                # print("D:")
-            else:
-                inter_var = var_maps[curr_token[1]]
-            
-            var_maps[curr_token[1]] = inter_var
-            if (curr_token in value_dic):
-                value_dic[curr_token[1]] += 1
-            else:
-                value_dic[curr_token[1]] = 1
+            if (tokens[i - 1][1] != "=" and tokens[i - 1][1] != "("):
+
+                token_exists = 0
+                for each in quadruples:
+                    if (curr_token[-1] in var_maps):
+                        if (each[-1] == var_maps[curr_token[1]]):
+                            token_exists = 1
+                            break
+
+                if (token_exists == 0):
+                    inter_var = "t"+ str(var_count)
+                    # print("D:")
+                else:
+                    inter_var = var_maps[curr_token[1]]
                 
+                var_maps[curr_token[1]] = inter_var
+                if (curr_token[1] in value_dic):
+                    value_dic[curr_token[1]] += 1
+                else:
+                    value_dic[curr_token[1]] = 1
+                    
 
-            curr_val = symbol_table[curr_token[1]]
+                curr_val = symbol_table[curr_token[1]]
 
-            if ("+" in curr_val[value_dic[curr_token[1]]]):
-                op = "+"
-            elif ("-" in curr_val[value_dic[curr_token[1]]]):
-                op = "-"
-            elif ("*" in curr_val[value_dic[curr_token[1]]]):
-                op = "*"
-            elif ("/" in curr_val[value_dic[curr_token[1]]]):
-                op = "/"
-            
-            if (len(curr_val[1].strip("")) == 1):
-                arg1 = curr_val[value_dic[curr_token[1]]]
-                arg2 = ""
-            else:
-                # What we got from the symbol table
-                val_list = curr_val[value_dic[curr_token[1]]].strip("").split(" ")
-                arg1 = var_maps[val_list[0]]
-                arg2 = val_list[-1]
-            
-            
-            quadruples.append((op,arg1,arg2,inter_var))
-            var_count += 1
-            # print(var_maps,curr_token)
-            insert_flag = False
+                if ("+" in curr_val[value_dic[curr_token[1]]]):
+                    op = "+"
+                elif ("-" in curr_val[value_dic[curr_token[1]]]):
+                    op = "-"
+                elif ("*" in curr_val[value_dic[curr_token[1]]]):
+                    op = "*"
+                elif ("/" in curr_val[value_dic[curr_token[1]]]):
+                    op = "/"
+                
+                if (len(curr_val[1].strip("")) == 1):
+                    arg1 = curr_val[value_dic[curr_token[1]]]
+                    arg2 = ""
+                else:
+                    # What we got from the symbol table
+                    val_list = curr_val[value_dic[curr_token[1]]].strip("").split(" ")
+                    arg1 = var_maps[val_list[0]]
+                    arg2 = val_list[-1]
+                
+                
+                quadruples.append((op,arg1,arg2,inter_var))
+                print(quadruples)
+                var_count += 1
+                # print(var_maps,curr_token)
+                op = ""
         elif(curr_token[0] == "RTRN_STMT"):
             op = "ret"
     for entry in quadruples:
         if (entry[0] == "="):
             final_string += entry[3] + " " + entry[0] + " "+entry[1]+"\n"
         elif (entry[0] == "ret"):
-            final_string += "return " + entry[1] 
+            final_string += "return " + entry[1] + "\n"
+        elif (entry[0] == "while"):
+            final_string += entry[1] + "\n"
         else:
             final_string += entry[3] +  " = " + entry[1]+" " + entry[0] + " " + entry[2]+"\n"
         
