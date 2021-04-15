bac4710b73f0fedfbd18729fdf523a7c080676dd
SirHappi <sri13.vinukonda@gmail.com>
2021-04-15 19:28:54 -0400

    backend re-attach WIP

--- backend.py
+++ backend.py
@@ -14,12 +14,11 @@ def generate(inter_code):
 
     # print('\n'.join(inter_code_arr))
 
-    register_count = 0
-    final_string = ""
-
     
+    final_string = ""
+    space_str = ""
+    register_count = 1
     register_map = {}
-
     # Registor desc = {"R0":("t71","t43",...),..."RN":("t8","t32",...)}
     registor_descriptor = {}
 
@@ -27,8 +26,104 @@ def generate(inter_code):
     address_descriptor = {}
 
     basic_blocks = get_basic_blocks(inter_code_arr)
+    # print(basic_blocks)
+
+    temp_b_b = basic_blocks
+    for k in range(len(temp_b_b)):
+        for l in range(len(temp_b_b[k])):
+            if(":" in temp_b_b[k][l]):
+                temp = basic_blocks[k].pop(l)
+                temp = temp.split(" ",1)
+                for element in temp:
+                    basic_blocks[k].insert(element)
+                print("temp: ",temp)
+
     print(basic_blocks)
-    i = 1
+    
+    for i in range(len(basic_blocks)):
+        for j in range(len(basic_blocks[i])):
+            curr_instr = basic_blocks[i][j].split(" ")
+            if (":" in curr_instr[0]):
+                final_string += curr_instr[0].strip() + "\n"
+  
+                # print(curr_instr)
+                if (curr_instr[2] == "="):
+                    # space_str += " "
+                    if (not(curr_instr[1] in register_map)):
+                        register_map[curr_instr[1]] = "R{}".format(register_count)
+                        register_count += 1
+
+                    final_string += "LD " +register_map[curr_instr[1]]
+                    if (curr_instr[3].isdigit()):
+                        final_string += ",#" + curr_instr[3] + "\n"
+                    else:
+                        final_string += "," + curr_instr[3] + "\n"
+                    space_str += " "
+                elif (curr_instr[1] == "if"):
+                    pass
+                elif (curr_instr[1] == "return"):
+                    pass
+            elif ("return" in curr_instr[0]):
+                final_string += "ST {},{}".format("RESULT",register_map[curr_instr[1]]) + "\n"
+            elif("if" in curr_instr[0]):
+                # t3 = t2-t1
+                arg1 = curr_instr[1]
+                arg2 = curr_instr[3]
+                final_string += "LD " + "R{},{}".format(len(register_map)+1,register_map[arg1])+"\n"
+                final_string += "LD " + "R{},{}".format(len(register_map)+2,register_map[arg2])+"\n"
+                final_string += "SUB " + "R{},R{},R{}".format(len(register_map)+1,len(register_map)+1,len(register_map)+2) +"\n"
+
+                # IF R1 = 0 means they are eq
+                # IF R1 > 0 means R1 is bigger
+                # Else R2 is bigger
+                if (curr_instr[2] == ">="):
+                    final_string += "BGE R{},R{},{}".format(len(register_map)+1,len(register_map)+2,curr_instr[-1] + "\n")
+
+
+            elif(curr_instr[1] == "="):
+                
+                if (len(curr_instr) == 3):
+                    # print("strict eq", curr_instr)
+
+                    if (not(curr_instr[0] in register_map)):
+                        register_map[curr_instr[0]] = "R{}".format(register_count)
+                        register_count += 1
+
+                    space_str += " "
+                    final_string += "LD " + register_map[curr_instr[0]]
+                    if (curr_instr[2].isdigit()):
+                        final_string += ",#" + curr_instr[2] + "\n"
+                    else:
+                        final_string += "," + curr_instr[2] + "\n"
+                else:
+                    # print("strict eq", curr_instr)
+                    if (not(curr_instr[0] in register_map)):
+                        register_map[curr_instr[0]] = "R{}".format(register_count)
+                        register_count += 1
+                    if (not(curr_instr[2] in register_map)):
+                        register_map[curr_instr[2]] = "R{}".format(register_count)
+                        register_count += 1
+                    space_str += " "
+                    op = "LD"
+                    if (curr_instr[3] == "+"):
+                        op = "ADD"
+                    elif (curr_instr[3] == "-"):
+                        op = "SUB"
+                    elif (curr_instr[3] == "*"):
+                        op = "MUL"
+                    elif (curr_instr[3] == "/"):
+                        op = "DIV"
+                    final_string += "{} {},{}".format(op,register_map[curr_instr[0]],register_map[curr_instr[2]])
+                    if (curr_instr[-1].isdigit()):
+                        final_string += ",#" + curr_instr[-1] + "\n"
+                    else:
+                        final_string += "," + curr_instr[-1] + "\n"
+                
+
+                
+        space_str = ""
+    print("final_string:\n"+final_string)
+
 
 
     return None
